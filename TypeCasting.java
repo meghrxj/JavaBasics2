e1928e1ce7ea3487925c757e59afb6023ddcc408
meghrxj <hello.meghrajjj@gmail.com>
2021-10-14 00:09:39 +0530

    Update TypeCasting.java

--- TypeCasting.java
+++ TypeCasting.java
@@ -2,6 +2,24 @@ public class TypeCasting {
     
     public static void main(String[] args) {
 
-        
+/*Typecasting is a method of changing 
+an entity from one data type to another.*/
+//int a = 0.5; //Type mismatch: cannot convert from double to int
+int i = (int) 0.5;
+System.out.println(i); 
+//output 0
+
+//Types
+byte a; 
+short b;
+char c;
+int d;
+long e;
+float f;
+double g = 500.1;
+
+int ex = (int) g;
+
+//pog it bro you wont get it yet ;/
     }
 }
