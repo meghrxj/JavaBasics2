fd5cf8b8c62a154ff81681d586ba754abba99e03
meghrxj <hello.meghrajjj@gmail.com>
2021-10-14 00:01:56 +0530

    updated modulus

--- Modulus.java
+++ Modulus.java
@@ -1,3 +1,25 @@
 public class Modulus {
     
+    public static void main(String[] args){
+
+
+        System.out.println(4 + 2);
+        System.out.println(4 - 2);
+        System.out.println(4 * 2);
+
+
+        System.out.println(15 % 4); //output 3
+
+        //deciamls
+        System.out.println(1.5 % 0.5);
+
+        
+
+
+
+
+
+
+
+    }
 }
